8e04a1a411148eba3b9a2fe085be6c4df9dc0cba
Jon Deaton <jdeaton@stanford.edu>
2016-09-26 16:37:20 -0700

    Fixed cross validation of one algorithm

--- scripts/cross_validate.py
+++ scripts/cross_validate.py
@@ -5,20 +5,23 @@ This script is for doing N-fold cross validation of the Phamer scoring algorithm
 '''
 
 import os
-import sys
-import time
 import argparse
+import fileIO
+import learning
 import kmer
 import phamer
 import warnings
 import logging
 import numpy as np
-from sklearn.metrics import roc_curve, auc
 import matplotlib
 warnings.simplefilter('ignore', UserWarning)
-matplotlib.use("Agg")
+try:
+    os.environ["DISPLAY"]
+except KeyError:
+    matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from pyqt_fit import kde
+from scipy import stats
 
 __version__ = 1.0
 __author__ = "Jonathan Deaton (jdeaton@stanford.edu)"
@@ -28,165 +31,147 @@ logging.basicConfig(format='[%(asctime)s][%(levelname)s][%(funcName)s] - %(messa
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-def make_distributions(positive_scores, negative_scores, file_name='distributions.svg'):
-    '''
-    This function makes nice looking distribution plots of postiive and negative_data scores
-    :param positive_scores: The scores for the positive data
-    :param negative_scores: The scores for the negative data
-    :param file_name: The filename to save the plot to
-    :return: None
-    '''
-    fig, ax = plt.subplots()
-    ll = min([min(negative_scores), min(positive_scores)])
-    ul = max([max(negative_scores), max(positive_scores)])
-    rng = ul - ll
-    xs = np.linspace(ll - 0.5 * rng, ul + 0.5 * rng, 1000)
-    est1 = kde.KDE1D(positive_scores[:])
-    est2 = kde.KDE1D(negative_scores[:])
-    ax.fill(xs, est1(xs), fc='blue', alpha=0.4, label='%d Phage (bw=%.3g)' % (len(positive_scores), est1.bandwidth))
-    ax.fill(xs, est2(xs), fc='red', alpha=0.4, label='%d Bacteria (bw=%.3g)' % (len(negative_scores), est2.bandwidth))
-    plt.legend(loc='best')
-    plt.grid(True)
-    plt.savefig(file_name)
+
+class cross_validator(object):
+
+    def __init__(self):
+
+        self.output_directory = "cross_validation"
+
+        self.roc_figsize = (9, 7)
+
+        self.positive_data = None
+        self.negative_data = None
+
+        self.positive_scores = None
+        self.negative_scores = None
+
+        self.N = 20
+
+        self.scoring_function = None
+
+    def cross_validate(self):
+        '''
+        This function is for cross validation of a scoring metric
+        :return: The scores for the gold standard positive and negative points
+        '''
+        positive_asmt = np.arange(self.positive_data.shape[0]) % self.N
+        negative_asmt = np.arange(self.negative_data.shape[0]) % self.N
+
+        np.random.shuffle(positive_asmt)
+        np.random.shuffle(negative_asmt)
+
+        self.positive_scores, self.negative_scores = np.array([]), np.array([])
+
+        for n in xrange(self.N):
+            logger.info('Iteration %d/%d' % (1 + n, self.N))
+            # Data segmentation, and sub-selection
+            where_positive = (positive_asmt == n)
+            where_negative = (negative_asmt == n)
+            scoring_data = np.vstack((self.positive_data[where_positive], self.negative_data[where_negative]))
+            pos_training_data = self.positive_data[np.invert(where_positive)]
+            neg_training_data = self.negative_data[np.invert(where_negative)]
+            # Sub-selection scoring
+            scores = self.scoring_function(scoring_data, pos_training_data, neg_training_data)
+
+            self.positive_scores = np.append(self.positive_scores, scores[:np.sum(where_positive)])
+            self.negative_scores = np.append(self.negative_scores, scores[np.sum(where_negative):])
+
+        logger.info("%d-fold cross validation complete." % self.N)
+        return self.positive_scores, self.negative_scores
 
 
-def make_ROC(positive_scores, negative_scores, file_name='xvalidation_ROC.svg'):
-    '''
-    This function makes a nice looking ROC curve
-    :param positive_scores: The positive scores
-    :param negative_scores: The negative scores
-    :param roc_filename: The name of the file to save the image to
-    :return: None
-    '''
-    fpr, tpr, roc_auc = predictor_performance(positive_scores, negative_scores)
-    plt.figure(figsize=(9, 7))
-    plt.plot(fpr, tpr, label='AUC = %0.3f' % roc_auc)
-    plt.plot([0, 1], [0, 1], 'k--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.0])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.legend(loc="lower right")
-    plt.savefig(file_name)
-    plt.grid(True)
-    plt.minorticks_on()
-    plt.grid(b=True, which='minor')
+    def plot_score_distributions(self):
+        '''
+        This function makes nice looking distribution plots of postiive and negative_data scores
+        :param positive_scores: The scores for the positive data
+        :param negative_scores: The scores for the negative data
+        :param file_name: The filename to save the plot to
+        :return: None
+        '''
+        fig, ax = plt.subplots()
+        ll = min([min(self.negative_scores), min(self.positive_scores)])
+        ul = max([max(self.negative_scores), max(self.positive_scores)])
+        logger.debug("x limits: %.2f, %.2f" % (ll, ul))
+        ranqe = ul - ll
+        x = np.linspace(ll - 0.2 * ranqe, ul + 0.2 * ranqe, 1000)
+        logger.debug("Performing KDE on scores...")
+
+        pdf_positive = stats.gaussian_kde(self.positive_scores)
+        pdf_negative = stats.gaussian_kde(self.negative_scores)
+        y_pos = pdf_positive(x)
+        y_neg = pdf_negative(x)
+
+        logger.debug("Filling curves...")
+        ax.fill_between(x, y_pos, facecolor='blue', alpha=0.4, label='%d positive' % len(self.positive_scores))
+        ax.fill_between(x, y_neg, facecolor='red', alpha=0.4, label='%d negative' % len(self.negative_scores))
+        plt.legend(loc='best')
+        plt.grid(True)
+        file_name = self.get_score_distribution_filename()
+        plt.savefig(file_name)
+        plt.close()
 
 
-def predictor_performance(positive_scores, negative_scores):
-    '''
-    This function calculates the performance of a prediction algorithm
-    :param positive_scores: The scores for gold standard positive datapoints
-    :param negative_scores: The scores for gold standard negative datapoints
-    :return: A tuple containing false positive rate, true positive rate, and ROC area under the curve.
-    '''
-    truth = np.append(np.ones(len(positive_scores)), np.zeros(len(negative_scores))).astype(bool)
-    predictions = np.append(positive_scores, negative_scores)
-    fpr, tpr, _ = roc_curve(truth, predictions)
-    roc_auc = auc(fpr, tpr)
-
-    return fpr, tpr, roc_auc
-
-
-def cross_validate(positive_data, negative_data, N, method='combo', eps=[0.01, 0.01], min_pts=[2, 2]):
-    '''
-    This function is for cross validation of the Phamer learnign algorithm
-    :param positive_data: The gold standard positive poitns
-    :param negative_data: The gold standard negative points
-    :param N: Number of iterations of cross validation
-    :param eps: DBSCAN distance threshold
-    :param min_pts: DBSCAN minimum points per cluster
-    :return: The scores for the gold standard positive and negative points
-    '''
-    positive_asmt = np.arange(positive_data.shape[0]) % N
-    negative_asmt = np.arange(negative_data.shape[0]) % N
-
-    np.random.shuffle(positive_asmt)
-    np.random.shuffle(negative_asmt)
-
-    positive_scores, negative_scores = np.array([]), np.array([])
-
-    tic = time.time()
-
-    for n in xrange(N):
-        logger.info('Iteration %d of %d' % (1 + n, N))
-        which_positive = positive_asmt == n
-        which_negative = negative_asmt == n
-
-        data = np.vstack((positive_data[which_positive], negative_data[which_negative]))
-        scores = phamer.score_points(data, positive_data[np.invert(which_positive)], negative_data[np.invert(which_negative)], method=method, eps=eps, min_samples=min_pts)
-
-        positive_scores = np.append(positive_scores, scores[:np.sum(which_positive)])
-        negative_scores = np.append(negative_scores, scores[np.sum(which_positive):])
-
-    timed = time.time() - tic
-    logger.info("N-Fold cross validation done. %dh %dm %ds" % (timed // 3600, (timed % 3600) // 60, timed % 60))
-    return positive_scores, negative_scores
+    def plot_ROC(self):
+        '''
+        This function makes a receiver operator characteristic curve plot
+        :return: None
+        '''
+        fpr, tpr, roc_auc = learning.predictor_performance(self.positive_scores, self.negative_scores)
+        plt.figure(figsize=self.roc_figsize)
+        plt.plot(fpr, tpr, label='AUC = %0.3f' % roc_auc)
+        plt.plot([0, 1], [0, 1], 'k--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.0])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.legend(loc="lower right")
+        plt.grid(True)
+        plt.minorticks_on()
+        plt.grid(b=True, which='minor')
+        file_name = self.get_roc_curve_filename()
+        plt.savefig(file_name)
+        plt.close()
 
 
-def test_cut_response(directory, N=5, eps=[0.1,0.1], min_pts=[2,2], file_name='cut_response.svg', method='combo'):
-    '''
-    This function tests the effect of sequence cut length on Phamer learning algorithm
-    :param directory: The directory that contains the k-mer count data for different sized cute
-    :param N: Number of iterations of DBSCAN
-    :param eps: DBSCAN distance threshold
-    :param min_pts: DBSCAN minimum points per cluster
-    :param filename: The filename to save the output image to
-    :return: A dictionary mapping cut length to ROC AUC values
-    '''
-    files = [os.path.join(directory, file) for file in os.listdir(directory)]
-    cuts = [2000, 5000, 7500, 10000, 100000]
-    aucs = []
-    for cut in cuts:
-        phage_file = [os.path.join(directory, file) for file in files if 'phage' in file and '_c%s_' % str(cut) in file][0]
-        bacteria_file = [os.path.join(directory, file) for file in files if 'bacteria' in file and '_c%s_' % str(cut) in file][0]
+    def cross_validate_all_algorithms(self):
+        '''
+        For validation all learning algorithms
+        :return: None
+        '''
 
-        phage_kmers = kmer.read_kmer_file(phage_file, normalize=True, old=True)[1]
-        bacteria_kmers = kmer.read_kmer_file(bacteria_file, normalize=True, old=True)[1]
+        plt.figure(figsize=self.roc_figsize)
+        for method in self.methods:
+            self.method = method
+            logger.info("%d-Fold Cross Validation with algorithm: %s" % (self.N, method.upper()))
+            positive_scores, negative_scores = self.cross_validate()
+            fpr, tpr, roc_auc = learning.predictor_performance(positive_scores, negative_scores)
+            plt.plot(fpr, tpr, label='%s - AUC = %0.3f' % (method.upper(), roc_auc))
 
-        logger.info("loaded k-mers from: %s and %s" % (os.path.basename(phage_file), os.path.basename(bacteria_file)))
-        phage_scores, bacteria_scores = cross_validate(phage_kmers, bacteria_kmers, N, eps=eps, min_pts=min_pts, method=method)
-        auc = predictor_performance(phage_scores, bacteria_scores)[2]
-        aucs.append(auc)
+        plt.plot([0, 1], [0, 1], 'k--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.0])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.legend(loc="lower right", title="Algorithm")
+        plt.grid(True)
+        plt.minorticks_on()
+        plt.grid(b=True, which='minor')
 
-    plt.figure(figsize=(9, 6))
-    plt.plot(np.array([0] + cuts) / 1000.0, [0] + aucs, 'b-o')
-    plt.grid(True)
-    plt.xlabel('Cut Size (kbp)')
-    plt.ylabel('ROC AUC')
-    plt.savefig(file_name)
-    return dict(zip(cuts, aucs))
+        file_name = self.get_combined_roc_curve_filename()
+        plt.savefig(file_name)
+        plt.close()
+
+    # filename makers
+    def get_score_distribution_filename(self):
+        return os.path.join(self.output_directory, 'score_distributions.svg')
+
+    def get_roc_curve_filename(self):
+        return os.path.join(self.output_directory, "roc.svg")
 
 
-def cross_validate_all(positive_data, negative_data, N,image_filename='combined_ROC.svg', output=''):
-    '''
-    For validation all learning algorithms
-    :param positive_data: The gold standard positive poitns
-    :param negative_data: The gold standard negative points
-    :param N: Number of iterations of cross validation
-    :return: None
-    '''
-    eps = [0.012, 0.012]
-    min_pts = [2, 2]
-    methods = ['dbscan', 'kmeans', 'knn', 'svm', 'density', 'combo']
-    plt.figure(figsize=(9, 7))
-
-    for method in methods:
-        logger.info("%d-Fold Cross Validation on: %s" % (N, method.upper()))
-        positive_scores, negative_scores = cross_validate(positive_data, negative_data, N, method=method)
-        fpr, tpr, roc_auc = predictor_performance(positive_scores, negative_scores)
-        plt.plot(fpr, tpr, label='%s - AUC = %0.3f' % (method.upper(), roc_auc))
-
-    plt.plot([0, 1], [0, 1], 'k--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.0])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.legend(loc="lower right", title="Learning Algorithm")
-    plt.grid(True)
-    plt.minorticks_on()
-    plt.grid(b=True, which='minor')
-    plt.savefig(image_filename)
+    def get_combined_roc_curve_filename(self):
+        return os.path.join(self.output_directory, "all_algorithms_roc.png")
 
 
 if __name__ == '__main__':
@@ -195,30 +180,21 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=script_description, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     input_group = parser.add_argument_group("Inputs")
-    input_group.add_argument('-p', '--positive', type=str, help='Positive k-mer counts')
-    input_group.add_argument('-n', '--negative', type=str, help='Negative k-mer counts')
-    input_group.add_argument('-cuts', '--cuts', type=str, help='analyse cut response from directory')
+    input_group.add_argument('-pf', '--positive_features_file', help='Positive features')
+    input_group.add_argument('-nf', '--negative_features_file', help='Negative features')
 
     output_group = parser.add_argument_group("Outputs")
-    output_group.add_argument('-out', '--output', type=str, default='', help='Output directory for plots')
+    output_group.add_argument('-out', '--output_directory', help='Output directory for plots')
 
     options_group = parser.add_argument_group("Options")
-    options_group.add_argument('-N', '--N_fold', default=5, type=int, help="Number of iteration in N-fold cross validation")
-    options_group.add_argument('-m', '--method', default='combo', type=str, help='Scoring algorithm method')
+    options_group.add_argument('-N', '--N_fold', default=20, type=int, help="Number of iteration in N-fold cross validation")
+    options_group.add_argument('-m', '--method', default='combo', help='Scoring algorithm method')
     options_group.add_argument('-a', '--CV_all', action='store_true', help='Flag to cross validate all algorithms')
 
     console_options_group = parser.add_argument_group("Console Options")
     console_options_group.add_argument('-v', '--verbose', action='store_true', default=False, help='Verbose output')
     console_options_group.add_argument('--debug', action='store_true', default=False, help='Debug console')
     args = parser.parse_args()
-
-    positive_file = args.positive
-    negative_file = args.negative
-    N = args.N_fold
-    method = args.method
-    all = args.CV_all
-    cuts = args.cuts
-    output = args.output
 
     if args.debug:
         logger.setLevel(logging.DEBUG)
@@ -230,28 +206,29 @@ if __name__ == '__main__':
         logger.setLevel(logging.WARNING)
         logging.basicConfig(format='[log][%(levelname)s] - %(message)s')
 
+    validator = cross_validator()
+    validator.scoring_function = phamer.score_points
+    validator.method = args.method
+    validator.N = args.N_fold
+    validator.method = args.method
+    validator.output_directory = args.output_directory
 
-    positive_data = kmer.read_kmer_file(positive_file)[1]
-    negative_data = kmer.read_kmer_file(negative_file)[1]
+    positive_ids, positive_data = fileIO.read_feature_file(args.positive_features_file)
+    negative_ids, negative_data = fileIO.read_feature_file(args.negative_features_file)
 
-    positive_data = kmer.normalize_counts(positive_data)
-    negative_data = kmer.normalize_counts(negative_data)
+    validator.positive_data = kmer.normalize_counts(positive_data)
+    validator.negative_data = kmer.normalize_counts(negative_data)
 
-    positive_scores, negative_scores = cross_validate(positive_data, negative_data, N, method=method)
+    validator.cross_validate()
 
-    logger.info("Making plots...")
-    distribution_file = os.path.join(output, 'distributions.svg')
-    make_distributions(positive_scores, negative_scores, file_name=distribution_file)
+    logger.info("Plotting score distributions...")
+    validator.plot_score_distributions()
+    logger.info("Plotting ROC curve...")
+    validator.plot_ROC()
 
-    roc_filename = os.path.join(output, 'xvalidation_ROC.svg')
-    make_ROC(positive_scores, negative_scores, file_name=roc_filename)
+    if args.CV_all:
+        logger.info("Validating all algorithms...")
+        validator.methods = ['dbscan', 'kmeans', 'knn', 'svm', 'density', 'combo']
+        validator.cross_validate_all_algorithms()
 
-    if cuts:
-        logger.info("Testing cut response... Directory: %s" % cuts)
-        cut_filename = os.path.join(output, 'cut_response.svg')
-        test_cut_response(cuts, N=N, method=method, file_name=cut_filename)
-
-    if all:
-        cross_validate_all(positive_data, negative_data, N, output=output)
-
-    logger.info("Cross Validation Complete")
+    logger.info("Cross validation complete.")
