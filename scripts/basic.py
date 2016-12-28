ae47983459641904d05c7dd901b4d23a831afe8e
jondeaton <jonpauldeaton@gmail.com>
2016-12-28 13:27:31 -0500

    Removed query_yes_no()

--- scripts/basic.py
+++ scripts/basic.py
@@ -37,40 +37,6 @@ def generate_summary(args, line_start='', header=''):
     return str_rep
 
 
-def query_yes_no(question, default="yes"):
-    """
-    Ask a yes/no question via raw_input() and return their answer.
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
-    The "answer" return value is True for "yes" or False for "no".
-    Created by Trent Mick on Fri, 19 Feb 2010 (MIT)
-    Taken from: http://code.activestate.com/recipes/577058/
-    """
-    valid = {"yes": True, "y": True, "ye": True,
-             "no": False, "n": False}
-    if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("Invalid default answer: '%s'" % default)
-
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = raw_input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "
-                             "(or 'y' or 'n').\n")
-
-
 def chop(array, chops):
     """
     This function is for separating the rows of a numpy array
