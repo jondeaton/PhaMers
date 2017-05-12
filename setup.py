f65a7f242cdf0cd5cfcaa50aeb49c363a8a95135
jondeaton <ghost@Jon.stanford.edu>
2017-05-11 17:50:18 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='PhaMers',
+    version='1.0',
+    packages=['phamers',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
